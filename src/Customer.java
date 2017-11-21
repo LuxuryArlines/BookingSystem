42e3e5e400d77fae82621631a2deb341badec36d
Staffan Arveng <arveng.staffan@gmail.com>
2017-11-21 09:42:08 +0100

    Added Customer class

---
+++ src/Customer.java
@@ -0,0 +1,13 @@
+
+public class Customer {
+	
+	private String firstName="";
+	private String lastName="";
+	private String emailAdress="";
+	
+	public Customer(String firstName,String lastName,String emailAdress) {
+		this.firstName= firstName;
+		this.lastName= lastName;
+		this.emailAdress=emailAdress;
+	}
+}
